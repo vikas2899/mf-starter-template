2cb5366345b5681a33b4c8696d000bd3e2fbeb5e
Vikas Pandey <43039761+vikas2899@users.noreply.github.com>
2023-12-20 11:29:25 +0530

    Add files via upload

---
+++ host/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from 'mfe1/Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>Host Up</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
