767b61f822f6385969d2554893a5cd30a589854f
Vikas Pandey <43039761+vikas2899@users.noreply.github.com>
2023-12-20 11:31:38 +0530

    Create webpack.dev.js

---
+++ mfe1/config/webpack.dev.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+
+const devConfig = {
+  mode: 'development',
+  output: {
+    publicPath: 'http://localhost:3001/',
+  },
+  devServer: {
+    port: 3001,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'mfe1',
+      filename: 'remoteEntry.js',
+      remotes: {},
+      exposes: {
+        './Counter': './src/components/Counter.js',
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+};
+
+module.exports = devConfig;
