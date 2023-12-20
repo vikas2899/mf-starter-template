11215783104a2c69a86cd01ef61d8bfdcd117ab3
Vikas Pandey <43039761+vikas2899@users.noreply.github.com>
2023-12-20 11:28:11 +0530

    Create webpack.dev.js

---
+++ host/config/webpack.dev.js
@@ -0,0 +1,56 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+
+const devConfig = {
+  mode: 'development',
+  output: {
+    publicPath: 'http://localhost:3000/',
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      filename: 'remoteEntry.js',
+      remotes: {
+        mfe1: 'mfe1@http://localhost:3001/remoteEntry.js',
+      },
+      shared: {
+        ...packageJson.dependencies,
+        react: {
+          singleton: true,
+          requiredVersion: packageJson.dependencies.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: packageJson.dependencies['react-dom'],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+};
+
+module.exports = devConfig;
